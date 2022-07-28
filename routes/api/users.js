148acfd39c4928983c6e8393c0bc66778d5ee741
Adewuyi uthman alao <adewuyiuthmanalao@gmail.com>
2022-07-28 20:22:57 +0100

    still on user registration and Auth, testing if user exist and getting user's gravatar and encrypting password using bcrypt

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const {check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const {check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
@@ -28,16 +30,41 @@ async (req, res) => {
     const { name, email, password } = req.body
 
     try {
+        //See if user exist
 
-    //See if user exist
+        let user = await User.findOne({email});
 
+        if(user){
+           return res.status(400).json({errors: [{msg: 'User already exist' }] });
+        }
+
+    
     //Get users gravatar
+
+    const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+    })
+
+    user = new User({
+        name,
+        email,
+        avatar,
+        password
+    });
 
     //Encrypt password
 
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
     //Return jsonwebtoken
     
-    res.send('User route');
+    res.send('User registered');
 
     } catch(err){
         console.error(err.message);
